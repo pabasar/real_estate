509477795f6f30c0f6a28600dc81f2476e18c47d
pabasar <pabsr.sur@gmail.com>
2020-10-14 16:24:10 +0530

    Context added

---
+++ RealEstateBusiness/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateBusiness.Models
+{
+    public class Branch
+    {
+        [Key]
+        public String BranchNo { get; set; }
+
+        public String Street { get; set; }
+
+        public String City { get; set; }
+
+        public int PostCode { get; set; }
+    }
+}
---
+++ RealEstateBusiness/Models/BusinessContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateBusiness.Models
+{
+    public class BusinessContext:DbContext
+    {
+        public DbSet<Branch> Branches { get; set; }
+    }
+}
