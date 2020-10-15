925a634c46d3a7b0f602344c26d4258c5c6943e8
pabasar <pabsr.sur@gmail.com>
2020-10-15 14:27:42 +0530

    context

--- RealEstateBusiness/Models/BusinessContext.cs
+++ RealEstateBusiness/Models/BusinessContext.cs
@@ -9,5 +9,8 @@ namespace RealEstateBusiness.Models
     public class BusinessContext:DbContext
     {
         public DbSet<Branch> Branches { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
     }
 }
