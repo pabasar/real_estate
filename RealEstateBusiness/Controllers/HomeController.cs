aa970268214654836574195d800c251d320fad49
pabasar <pabsr.sur@gmail.com>
2020-10-21 20:24:11 +0530

    Staff

---
+++ RealEstateBusiness/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using RealEstateBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstateBusiness.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BusinessContext campusContext = new BusinessContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
