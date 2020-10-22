0e1133d47aece45261573919730d99fe7305ddbb
pabasar <pabsr.sur@gmail.com>
2020-10-22 10:39:03 +0530

    Home controller update

--- RealEstateBusiness/Controllers/HomeController.cs
+++ RealEstateBusiness/Controllers/HomeController.cs
@@ -9,11 +9,43 @@ namespace RealEstateBusiness.Controllers
 {
     public class HomeController : Controller
     {
-        private BusinessContext campusContext = new BusinessContext();
+        private BusinessContext businessContext = new BusinessContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<Staff> Staffs = businessContext.Staffs.ToList();
+            return View(Staffs);
+        }
+
+        public ActionResult Branches()
+        {
+            List<Branch> AllBranches = businessContext.Branches.ToList();
+            return View(AllBranches);
+        }
+
+        public ActionResult StaffNames()
+        {
+            List<Staff> StaffName = businessContext.Staffs.ToList();
+            return View(StaffName);
+        }
+
+        public ActionResult StaffDetails(String id)
+        {
+            Staff staff = businessContext.Staffs
+                .SingleOrDefault(x => x.StaffNo == id);
+            return View(staff);
+        }
+
+        public ActionResult Branch()
+        {
+            List<Branch> branches = businessContext.Branches.ToList();
+            return View(branches);
+        }
+
+        public ActionResult StaffInBranch(String BranchNo)
+        {
+            List<Staff> staffs = businessContext.Staffs.Where(x => x.BranchRef == BranchNo).ToList();
+            return View(staffs);
         }
     }
 }
